55e5155a63aa0da8ab998a938ff79f9d538b8a49
djin123 <502037675@qq.com>
2020-04-14 16:09:29 +0800

    修改TestB类中的代码，添加demo02方法，第2次提交。。

--- src/main/java/cn/com/djin/springboot/interceptor/TestB.java
+++ src/main/java/cn/com/djin/springboot/interceptor/TestB.java
@@ -8,4 +8,12 @@ public class TestB {
     public void demo01(){
         System.out.println("测试类testB的demo01方法。。");
     }
+
+    //组员A写方法demo02
+    public void demo02(){
+        System.out.println("测试方法demo02.。");
+    }
+
+
+
 }
